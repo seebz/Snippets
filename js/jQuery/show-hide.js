11cab629ca85b7b90b508d3e2035c484687114f6
Seebz <sebastien@seebz.net>
2011-12-20 11:25:43 +0100

    Ajout des functions show/hide avec support des <option> sous IE

---
+++ js/jQuery/show-hide.js
@@ -0,0 +1,51 @@
+
+jQuery(function(){
+	
+	// Amélioration de show/hide pour pouvoir fonction avec des <option> sous IE
+	if (jQuery.browser.msie) {
+		var ___show = jQuery.fn.show;
+		jQuery.fn.show = function() {
+			var self = this;
+			var args = arguments;
+			this.each(function() {
+				if (jQuery(this).is('option')) {
+					// <option>
+					var parent = jQuery(this).parent(), elem = jQuery(this);
+					if (jQuery(parent).is('span')) {
+						jQuery(parent).replaceWith(elem);
+					}
+				} else if (jQuery(this).is('span') && jQuery(this).find(option).length) {
+					// hidden <option>
+					var elem = jQuery('option', jQuery(this));
+					jQuery(this).replaceWith(elem);
+				} else {
+					// other element
+					var elem = jQuery(this);
+				}
+				___show.apply(elem, args);
+			});
+			return self;
+		}
+		
+		var ___hide = jQuery.fn.hide;
+		jQuery.fn.hide = function() {
+			var self = this;
+			var args = arguments;
+			jQuery(this).each(function() {
+				if (!jQuery(this).is('option')) {
+					// other element
+					var elem = jQuery(this);
+				} else if (!jQuery(this).parent().is('span')) {
+					// visible <option>
+					var elem = jQuery(this).wrap('<span>');
+				} else {
+					// hidden <option>
+					var elem = jQuery(this).parent();
+				}
+				___hide.apply(elem, args);
+			});
+			return self;
+		};
+	}
+	
+});
